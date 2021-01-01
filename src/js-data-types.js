884ead6f7386afdf3fd6d4c100ac9043c19ba8b8
agatalts <agata.e.latos@gmail.com>
2021-01-01 18:41:32 +0000

    Changed current time to last updated time and small css changes

--- src/js-data-types.js
+++ src/js-data-types.js
@@ -1,29 +1,34 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
 
-  let now = new Date();
-  let daysWeek = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday"
-];
-let currentDay = daysWeek[now.getDay()];
-let currentHour = now.getHours();
-if (currentHour < 10) {
-    currentHour = `0${currentHour}`;
-  }
-let currentMinutes = now.getMinutes();
-if (currentMinutes < 10) {
-    currentMinutes = `0${currentMinutes}`;
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${formatHours(timestamp)}`;
 }
-let currentDayWeek = document.querySelector("li.current-day");
 
-currentDayWeek.innerHTML = `${currentDay}`
+function formatHours(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
 
-let currentTime = document.querySelector("li.current-time");
-currentTime.innerHTML = `${currentHour}:${currentMinutes}`;
+  return `${hours}:${minutes}`;
+}
+  
+  
 
 
 //function showFahrenheit(event) {
@@ -59,6 +64,7 @@ function showConditions(response) {
   document.querySelector("#humidity").innerHTML = response.data.main.humidity;
   document.querySelector("#wind").innerHTML = Math.round(response.data.wind.speed);
   document.querySelector("h4").innerHTML = response.data.weather[0].main;
+  document.querySelector("li.day-and-time").innerHTML = formatDate(response.data.dt * 1000);
 }
 
 function showCity(event) {
